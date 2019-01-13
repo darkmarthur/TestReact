12e7e10ee6d88a634aa30404fc6b3448dea0b4f4
mdominguez <darkmarthur@gmail.com>
2019-01-13 01:05:58 -0600

    .

--- wheather-app/src/components/WeatherLocation/index.js
+++ wheather-app/src/components/WeatherLocation/index.js
@@ -1,29 +1,59 @@
-import React from 'react';
-import Location from './Location';
-import WeatherData from './WeatherData';
+import React, { Component } from "react";
+import Location from "./Location";
+import WeatherData from "./WeatherData";
 import "./styles.css";
 import {
-    CLOUD,
-    CLOUDY,
-    SUN,
-    RAIN,
-    SNOW,
-    WINDY,
-    THUNDER,
-    DRIZZLE
-  } from '../../constants/weathers';
-  
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY,
+  THUNDER,
+  DRIZZLE
+} from "../../constants/weathers";
+
 const data = {
-    temperature: 235,
-    weatherState: SUN,
-    humidity: 10,
-    wind: '10 m/s'
+  temperature: 235,
+  weatherState: SUN,
+  humidity: 10,
+  wind: "10 m/s"
+};
+
+const data2 = {
+  temperature: 211,
+  weatherState: WINDY,
+  humidity: 13,
+  wind: "14 m/s"
+};
+
+class WeatherLocation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      city: "Mexa",
+      data: data
+    };
+  }
+
+  handleUpdateClick = () => {
+    this.setState({
+      city: "CDMX",
+      data: data2
+    });
+    console.log("hi");
+  };
+
+  render() {
+    const { city, data } = this.state;
+    return (
+      <div className="weatherLocationCont">
+        <Location city={city} />
+        <WeatherData data={data} />
+        <button onClick={this.handleUpdateClick}>Update</button>
+      </div>
+    );
+  }
 }
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={"Santiago"}></Location>
-        <WeatherData data={data}></WeatherData>
-    </div>
-);
 
 export default WeatherLocation;
